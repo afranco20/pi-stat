240b66c8cb212fbd49045ad114fdf103aa40549f
Alexander Franco <26696700+afranco20@users.noreply.github.com>
2020-10-13 04:24:46 -0400

    :sparkles: add cpu temperature

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::process::Command;
 
 fn main() {
     uptime();
+    cpu_temp();
 }
 
 fn uptime() {
@@ -11,4 +12,17 @@ fn uptime() {
         .expect("failed to get uptime");
 
     println!("{}", String::from_utf8_lossy(&uptime.stdout));
+}
+
+fn cpu_temp() {
+    let temp = Command::new("cat")
+        .arg("/sys/class/thermal/thermal_zone0/temp")
+        .output()
+        .expect("failed to get cpu temp");
+
+    let val: String = String::from_utf8(temp.stdout).expect("not valid utf8");
+    let val: i32 = val.trim().parse().expect("not a valid number");
+
+    println!("CPU Temp: {} °C", val / 1000);
+    // println!("{}", temp.status);
 }
