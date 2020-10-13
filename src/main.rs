a536e3caf0197fb73e180d4aec9294d177271e9f
Alexander Franco <26696700+afranco20@users.noreply.github.com>
2020-10-13 04:37:13 -0400

    :sparkles: add cpu load average

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use regex::Regex;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
+use std::io::Result;
 use std::process::Command;
 
 fn main() {
     uptime();
     cpu_temp();
+    let _ = cpu_load_average();
 }
 
 fn uptime() {
@@ -25,4 +31,33 @@ fn cpu_temp() {
 
     println!("CPU Temp: {} °C", val / 1000);
     // println!("{}", temp.status);
+}
+
+fn parse_load_average(load_average: &String) -> Option<(f32, f32, f32, i32, i32, i32)> {
+    let regex = Regex::new(r"[\d\.]+").unwrap();
+    let mut caps = regex.find_iter(load_average);
+
+    let avg1 = caps.next().unwrap().as_str().parse().ok()?;
+    let avg5 = caps.next().unwrap().as_str().parse().ok()?;
+    let avg15 = caps.next().unwrap().as_str().parse().ok()?;
+    let active_procs = caps.next().unwrap().as_str().parse().ok()?;
+    let total_procs = caps.next().unwrap().as_str().parse().ok()?;
+    let last_proc = caps.next().unwrap().as_str().parse().ok()?;
+
+    return Some((avg1, avg5, avg15, active_procs, total_procs, last_proc));
+}
+
+fn cpu_load_average() -> Result<()> {
+    let file = File::open("/proc/loadavg")?;
+    let mut buf_reader = BufReader::new(file);
+
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+
+    let res = parse_load_average(&contents).unwrap();
+
+    // println!("{:#?}", res);
+    println!("{:.2} {:.2} {:.2}", res.0, res.1, res.2);
+
+    return Ok(());
 }
